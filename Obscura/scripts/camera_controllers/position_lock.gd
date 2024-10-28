e881bb802ec2e79bee6fba02bdc69e7aa136023d
patple <152749587+patple@users.noreply.github.com>
2024-10-28 11:35:26 -0700

    stage 1
    it now works and the cross is now visible

--- Obscura/scripts/camera_controllers/position_lock.gd
+++ Obscura/scripts/camera_controllers/position_lock.gd
@@ -1,14 +1,14 @@
 class_name PositionLock
 extends CameraControllerBase
 
-#@export var vert_cross_length:float = 5.0
-#@export var horz_cross_length:float = 5.0
-
+@export var vert_cross_length:float = 5.0
+@export var horiz_cross_length:float = 5.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	super()
-	position = target.position
+	position = Vector3(target.position.x, dist_above_target, target.position.z)
+	rotation_degrees = Vector3(-90, 0, 0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,7 +16,9 @@ func _process(delta: float) -> void:
 	if !current: 
 		return 
 	
-	position = target.position
+	position = Vector3(target.position.x, dist_above_target, target.position.z)
+	
+	super(delta)
 	
 	if draw_camera_logic:
 		draw_logic()
@@ -35,19 +37,19 @@ func draw_logic() -> void:
 	var size_cross = 5
 	immediate_mesh.surface_begin(Mesh.PRIMITIVE_LINES, material)
 	
-	immediate_mesh.surface_add_vertex(Vector3(-size_cross, 0, 0))
-	immediate_mesh.surface_add_vertex(Vector3(size_cross, 0, 0))
+	immediate_mesh.surface_add_vertex(Vector3(-horiz_cross_length, 0, 0))
+	immediate_mesh.surface_add_vertex(Vector3(horiz_cross_length, 0, 0))
 	
-	immediate_mesh.surface_add_vertex(Vector3(0, -size_cross, 0))
-	immediate_mesh.surface_add_vertex(Vector3(0, size_cross, 0))
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, -vert_cross_length))
+	immediate_mesh.surface_add_vertex(Vector3(0, 0, vert_cross_length))
 	
 	immediate_mesh.surface_end()
-
 	material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED
 	material.albedo_color = Color.BLACK
 	
 	add_child(mesh_instance)
-	mesh_instance.global_position = global_position
+	mesh_instance.global_transform = Transform3D.IDENTITY
+	mesh_instance.global_position = Vector3(target.position.x, dist_above_target, target.position.z)
 	
 	await get_tree().process_frame
 	mesh_instance.queue_free()
